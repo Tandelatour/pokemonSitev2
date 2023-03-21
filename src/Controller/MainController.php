58274823a78eb7848160c7d31ee8fa6312dc5594
alatour2022 <tandelatour@gmail.com>
2023-03-21 12:06:02 +0100

    maj 21/03

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_accueil')]
@@ -49,17 +50,48 @@ class MainController extends AbstractController
     //redirect
 
     #[Route('/pokemon/capture/{id}', name: 'main_capture')]
-    public function capturer(int $id, PokemonRepository $pokemonRepository): Response
+    public function capturer(int $id, PokemonRepository $pokemonRepository, EntityManagerInterface $entityManager): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
+
         $pokemon = $pokemonRepository->find($id);
         $pokemon->setEstCapture(!$pokemon->isEstCapture());
         $entityManager->persist($pokemon);
         $entityManager->flush();
 
-
         return $this->redirectToRoute('main_pokedex', [
-
         ]);
     }
+
+    #[Route('/pokemon/tri/{typeTrie}', name: 'main_tri_alpha')]
+    public function trierAlpha(PokemonRepository $pokemonRepository,string $typeTrie ): Response
+    {
+        $direction = $typeTrie === 'asc' ? 'ASC' : 'DESC';
+        $pokedex = $pokemonRepository->findBy([],['nom'=>$direction]);
+
+        return $this->render('main/pokedex.html.twig', [
+            "pokedex"=>$pokedex
+        ]);
+    }
+
+
+    #[Route('/pokemon/tricapture/{tri}', name: 'main_tri_capt')]
+    public function trierCapturer(PokemonRepository $pokemonRepository,int $tri ): Response
+    {
+        if($tri == 1){
+            $pokedex = $pokemonRepository->findBy(['est_capture'=>true]);
+        }else{
+            $pokedex = $pokemonRepository->findBy(['est_capture'=>false]);
+        }
+
+        return $this->render('main/pokedex.html.twig', [
+            "pokedex"=>$pokedex
+        ]);
+    }
+
+
+
+
+
+
+
 }
